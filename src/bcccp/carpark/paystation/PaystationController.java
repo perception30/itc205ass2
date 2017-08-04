dc7ac1e6014d0f3e4e499ff4fec28ceadac07611
Abrody01 <Abrody1188@gmail.com>
2017-08-05 07:55:40 +1000

    Updated PaystationController

--- src/bcccp/carpark/paystation/PaystationController.java
+++ src/bcccp/carpark/paystation/PaystationController.java
@@ -2,35 +2,78 @@ package bcccp.carpark.paystation;
 
 import bcccp.carpark.ICarpark;
 import bcccp.tickets.adhoc.IAdhocTicket;
+import bcccp.tickets.adhoc.IAdhocTicketDAO;
+
+
+
 
 public class PaystationController 
 		implements IPaystationController {
 	
 	private IPaystationUI ui;	
 	private ICarpark carpark;
-
-	private IAdhocTicket  adhocTicket = null;
+        private IAdhocTicketDAO AdhocTicketDAO;
+	private IAdhocTicket adhocTicket = null;
 	private float charge;
 	
 	
 
 	public PaystationController(ICarpark carpark, IPaystationUI ui) {
 		//TODO Implement constructor
+                this.carpark = carpark;
+                this.ui = ui;
 	}
 
 
 
 	@Override
 	public void ticketInserted(String barcode) {
-		// TODO Auto-generated method stub
 		
-	}
+      
+            adhocTicket = AdhocTicketDAO.findTicketByBarcode(barcode);
+            if(adhocTicket != null)
+            {
+                if(adhocTicket.isCurrent())
+                {
+                    if(!adhocTicket.hasExited())
+                    {
+                        if(!adhocTicket.isPaid())
+                        {
+                            charge = carpark.calculateAddHocTicketCharge(adhocTicket.getEntryDateTime());
+                            ui.display("Charge: " + String.valueOf(charge));
+
+                        }
+                        else
+                        {
+                            ui.display("Ticket has already been paid");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                     ui.display("Ticket has already been used to exit carpark");
+                     return;
+                    }
+                }
+                ui.display("ticket is not current");
+            }
+            else 
+            {
+            ui.display("ticket is not registered for this carpark");
+            }
+ }
 
 
 
 	@Override
 	public void ticketPaid() {
 		// TODO Auto-generated method stub
+            long currentTimeMillis = System.currentTimeMillis();
+            charge = adhocTicket.getCharge();
+            adhocTicket.pay(currentTimeMillis, charge);
+            ui.printTicket(adhocTicket.getCarparkId(), adhocTicket.getTicketNo(), adhocTicket.getEntryDateTime(), adhocTicket.getPaidDateTime(), charge, adhocTicket.getBarcode());
+            ui.display("Thank you ");
+                    
 		
 	}
 
@@ -38,6 +81,9 @@ public class PaystationController
 
 	@Override
 	public void ticketTaken() {
+            
+            ui.display("Please insert ticket to begin payment");
+            
 		// TODO Auto-generated method stub
 		
 	}
@@ -45,3 +91,4 @@ public class PaystationController
 	
 	
 }
+
