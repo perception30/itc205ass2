3d75b07189d3e12d8034976c60504ac07ef76843
Jmz01 <jmz_01@live.com>
2017-08-10 16:03:14 +1000

    Update ExitController.java

--- bcccp/carpark/exit/ExitController.java
+++ bcccp/carpark/exit/ExitController.java
@@ -7,9 +7,7 @@ import bcccp.carpark.ICarpark;
 import bcccp.carpark.IGate;
 import bcccp.tickets.adhoc.IAdhocTicket;
 
-public class ExitController 
-		implements ICarSensorResponder,
-		           IExitController {
+public class ExitController implements ICarSensorResponder, IExitController {
 	
 	private IGate exitGate;
 	private ICarSensor insideSensor;
@@ -17,40 +15,81 @@ public class ExitController
 	private IExitUI ui;
 	
 	private ICarpark carpark;
-	private IAdhocTicket  adhocTicket = null;
+	private IAdhocTicket adhocTicket = null;
 	private long exitTime;
 	private String seasonTicketId = null;
-	
-	
+        private boolean ticketValidated = false;  //added this to make things easier
+        
+	public ExitController(Carpark carpark, IGate exitGate, ICarSensor is, ICarSensor os, IExitUI ui) {
+		this.exitGate = exitGate;
+                ui.registerController(this);
+                this.ui = ui;
+                is.registerResponder(this);
+                os.registerResponder(this);
+                this.carpark = carpark;
+                this.insideSensor = is;
+                this.outsideSensor = os;
+        }
 
-	public ExitController(Carpark carpark, IGate exitGate, 
-			ICarSensor is,
-			ICarSensor os, 
-			IExitUI ui) {
-		//TODO Implement constructor
-	}
-
-
-
-	@Override
+        @Override
 	public void ticketInserted(String ticketStr) {
-		// TODO Auto-generated method stub
-		
-	}
+            if (ticketStr.contains("S")) {
+                seasonTicketId = ticketStr;
+                if (carpark.isSeasonTicketInUse(seasonTicketId) && carpark.isSeasonTicketValid(seasonTicketId)) {
+                    ui.display("Take Ticket");
+                    ticketValidated = true;
+                    ui.beep();
+                }
+                else {
+                    ui.display("Take Rejected Ticket");
+                }
+            }
+            else {
+                adhocTicket = carpark.getAdhocTicket(ticketStr);
+                if (adhocTicket.isCurrent() && adhocTicket.isPaid()) {
+                    ui.display("Take Ticket");
+                    ticketValidated = true;
+                    ui.beep();
+                }
+                else {
+                    ui.display("Take Rejected Ticket");
+                }
+            }
+        }
 
-
-
-	@Override
+        @Override
 	public void ticketTaken() {
-		// TODO Auto-generated method stub
-		
+            if (ticketValidated) {
+                exitGate.raise();
+                ticketValidated = false;
+            }
 	}
 
-
-
-	@Override
+        @Override
 	public void carEventDetected(String detectorId, boolean detected) {
-		// TODO Auto-generated method stub
+            if (detectorId.equals(insideSensor.getId()) && detected) {
+                ui.display("Insert Ticket");
+            }
+            if (detectorId.equals(outsideSensor.getId()) && detected) {
+                ui.display("Car Leaving");
+            }
+            if (detectorId.equals(insideSensor.getId()) && !detected) {
+                ui.display("Car Left");
+            }
+            if (detectorId.equals(outsideSensor.getId()) && !detected) {
+                exitGate.lower();
+                ui.display("");
+                exitTime = System.currentTimeMillis();
+                if (adhocTicket != null) {
+                    adhocTicket.exit(exitTime);
+                    adhocTicket = null;
+                }
+                else {
+                    carpark.recordSeasonTicketExit(seasonTicketId);
+                    seasonTicketId = null;
+                }
+                
+            }
 		
 	}
 
