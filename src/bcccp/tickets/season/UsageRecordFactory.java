22d0ef82788ae898b27d0cde42787cadcba92cb8
Abrody01 <Abrody1188@gmail.com>
2017-08-11 22:27:56 +1000

    Final Commit of Usage Factory

--- src/bcccp/tickets/season/UsageRecordFactory.java
+++ src/bcccp/tickets/season/UsageRecordFactory.java
@@ -4,7 +4,7 @@ public class UsageRecordFactory implements IUsageRecordFactory {
 
 	@Override
 	public IUsageRecord make(String ticketId, long startDateTime) {
-		// TODO Auto-generated method stub
+		
                 UsageRecord ur = new UsageRecord(ticketId, startDateTime);
 		return ur;
 	}
